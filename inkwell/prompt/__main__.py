fb0632e494982f280d81340dc9b225a65a243772
Shotaro Kohama <khmshtr28@gmail.com>
2023-08-21 22:28:44 -0700

    feat: add an artist name to the prompt

--- inkwell/prompt/__main__.py
+++ inkwell/prompt/__main__.py
@@ -1,13 +1,25 @@
+import random
+
 import polars as pl
 
 
 # See https://huggingface.co/datasets/Gustavosta/Stable-Diffusion-Prompts
 PARQUET_FILE_URL = "https://huggingface.co/datasets/Gustavosta/Stable-Diffusion-Prompts/resolve/main/data/train.parquet"
 
+FAVORITE_ARTISTS = [
+    "Charles Angrand",
+    "Van Gogh",
+    "Jackson Pollock",
+    "Francis Bacon",
+]
+
 
 def main():
+    artist = random.choice(FAVORITE_ARTISTS)
+    suffix = f"Artwork painted by {artist}."
+
     df = pl.read_parquet(PARQUET_FILE_URL)
-    print(df.sample(1)[0, "Prompt"])
+    print(df.sample(1)[0, "Prompt"], suffix)
 
 if __name__ == "__main__":
     main()
