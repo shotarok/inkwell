1765ff2bd028b3eeedea8e8bd8d4a1e408a1a0cd
Shotaro Kohama <khmshtr28@gmail.com>
2023-09-04 11:11:42 -0700

    Update the huggingface dataset for prompts

--- inkwell/prompt/__main__.py
+++ inkwell/prompt/__main__.py
@@ -1,13 +1,13 @@
 import polars as pl
 
 
-# See https://huggingface.co/datasets/Gustavosta/Stable-Diffusion-Prompts
-PARQUET_FILE_URL = "https://huggingface.co/datasets/Gustavosta/Stable-Diffusion-Prompts/resolve/main/data/train.parquet"
+# See https://huggingface.co/datasets/poloclub/diffusiondb
+PARQUET_FILE_URL = "https://huggingface.co/datasets/poloclub/diffusiondb/raw/main/metadata.parquet"
 
 
 def main():
     df = pl.read_parquet(PARQUET_FILE_URL)
-    print(df.sample(1)[0, "Prompt"])
+    print(df.sample(1)[0, "prompt"])
 
 if __name__ == "__main__":
     main()
